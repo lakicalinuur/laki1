d6610732ecf7429b1defb97455e29dc56ea8b78e
lakicalinuur <lakicalinuur@gmail.com>
2025-08-07 18:19:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
-TOKEN = "7790991731:AAFv2QQENqSe4rXiV7g6SFKOqeFIKmYogrA"
+TOKEN = "7790991731:AAF4NHGm0BJCf08JTdBaUWKzwfs82_Y9Ecw"
 ADMIN_ID = 6964068910
 WEBHOOK_URL = "https://laki1.onrender.com"
 
