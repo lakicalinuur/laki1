6ca1eeba9cda3899f1ddc2cc1ffcb480f6da3c25
lakicalinuur <lakicalinuur@gmail.com>
2025-08-11 16:23:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logging.basicConfig(
 )
 
 TOKEN = "7790991731:AAF4NHGm0BJCf08JTdBaUWKzwfs82_Y9Ecw"
-ADMIN_ID = 6964068910
+ADMIN_ID = 5978150981
 WEBHOOK_URL = "https://probable-sybilla-wwmahe-52df4f73.koyeb.app/"
 REQUIRED_CHANNEL = "@guruubka_wasmada"
 
